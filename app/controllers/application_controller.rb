246ee34b430329667d4fe3e1186a0febc7068a5c
suhail-n <suhail.nooristani@gmail.com>
2025-09-12 00:20:18 -0400

    add devise views.\n- completed signin and signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  layout :layout_by_resource
+
+  private
+  def layout_by_resource
+    if controller_name == "landing"
+      "landing"
+    elsif devise_controller?
+      # return "auth" if new_user_session GET    /users/sign_in(.:format) devise/sessions#new,  new_user_registration GET    /users/sign_up(.:format) devise/registrations#new, passwords#new
+      "landing" if (controller_name == "sessions" && action_name == "new") || (controller_name == "registrations" && action_name == "new") || (controller_name == "passwords" && action_name == "new")
+    else
+      "application"
+    end
+  end
 end
