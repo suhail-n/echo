b6eacf845fb6c2ae175552fa0f8d7921301dc127
suhail-n <suhail.nooristani@gmail.com>
2025-09-19 19:45:46 -0400

    bugfix: landing_controller reroute to correct workout_plans_pat

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -7,6 +7,6 @@ class LandingController < ApplicationController
   private
 
   def redirect_if_logged_in
-    redirect_to workout_plan_path if user_signed_in?
+    redirect_to workout_plans_path if user_signed_in?
   end
 end
