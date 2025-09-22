7f34b9f55f078f190b160d77b47179111e8d68f9
suhail-n <suhail.nooristani@gmail.com>
2025-09-21 22:41:42 -0400

    fix enum type syntax

--- app/models/exercise_muscle.rb
+++ app/models/exercise_muscle.rb
@@ -2,7 +2,7 @@ class ExerciseMuscle < ApplicationRecord
   belongs_to :exercise
   belongs_to :muscle
 
-  enum role: { primary: "primary", secondary: "secondary" }, _suffix: true
+  enum :role, { primary: "primary", secondary: "secondary" }, suffix: true
   validates :role, presence: true, inclusion: { in: roles.keys }
   validates :exercise_id, uniqueness: { scope: [ :muscle_id, :role ] }
 end
